0a4191fa0a34d5ad856fffca5cc9f75e7024d07f
Ashvin Tiwari <ashvintiwari161@gmail.com>
2025-11-03 12:22:09 +0530

    clean app.js

--- backend/app.js
+++ backend/app.js
@@ -9,12 +9,11 @@ import { admin } from "./src/firebase.js";
 const app = express();
 const server = createServer(app);
 
-// ✅ Set up Socket.IO with Firestore
 connectToSocket(server);
 
 app.set("port", process.env.PORT || 8000);
 
-// ✅ Middleware
+
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST"],
@@ -24,10 +23,8 @@ app.use(cors({
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
-// ✅ Routes
 app.use("/api/v1/users", userRoute);
 
-// ✅ Start Server
 server.listen(app.get("port"), () => {
   console.log(`🚀 Server listening on port ${app.get("port")}`);
 });
