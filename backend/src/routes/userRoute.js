8e3b54afaa81b7842efc45987b3310eda02d8f52
Ashvin Tiwari <ashvintiwari161@gmail.com>
2025-11-03 12:25:30 +0530

    userRoute.js

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -10,19 +10,14 @@ import {
 
 const router = Router();
 
-// User login
 router.post("/login", login);
 
-// User registration
 router.post("/register", register);
 
-// Google authentication
 router.post("/google-auth", googleAuth);
 
-// Add meeting to user's activity history
 router.post("/add_to_activity", addToHistory);
 
-// Get all activity for user
 router.get("/get_all_activity", getUserHistory);
 
 router.get("/verify-token", verifyToken);
